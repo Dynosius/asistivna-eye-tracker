2222c5d8eeceac8b1ff79c57427f8c90995e9b5f
yosukemi <josip.milotic@gmail.com>
2020-01-11 11:10:18 +0100

    Replaced C# with GDScript

---
+++ Scripts/BackButton.gd
@@ -0,0 +1,7 @@
+extends TextureButton
+
+func _ready():
+	pass 
+
+func _pressed():
+	get_tree().change_scene("res://Scenes/MainMenu.tscn")
---
+++ Scripts/ExitButton.gd
@@ -0,0 +1,7 @@
+extends TextureButton
+
+func _ready():
+	pass 
+	
+func _pressed():
+	get_tree().quit()
